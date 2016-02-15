dace050830f5ce6b673bc19540bc26ebf2dedc67
Todd Motto <todd@toddmotto.com>
2016-02-15 12:56:52 +0000

    chore: Added tests

---
+++ tests/ContactController.spec.js
@@ -0,0 +1,45 @@
+describe('ContactController', function () {
+	var $controller;
+
+	beforeEach(module('app'));
+
+	beforeEach(inject(function (_$controller_) {
+		$controller = _$controller_;
+	}));
+
+
+	it('should call $timeout with a delay of 5000 milliseconds', function () {
+		var $scope = {};
+		var time;
+
+		function mockTimeout(func, milliseconds) {
+			time = milliseconds;
+		}
+
+		$controller('ContactController as vm', { $scope: $scope, $timeout: mockTimeout });
+
+		expect(time).toBe(5000);
+	});
+
+	it('should have a name property on $scope', function () {
+		var $scope = {};
+
+		$controller('ContactController as vm', { $scope: $scope });
+
+		expect($scope.name).not.toBe(undefined);
+	});
+	
+	it('should update $scope.name after 5000 milliseconds', function () {
+		var $scope = {};
+		var originalName;
+
+		function mockTimeout(func, milliseconds) {
+			originalName = $scope.name;
+			func();
+		}
+
+		$controller('ContactController as vm', { $scope: $scope, $timeout: mockTimeout });
+
+		expect(originalName).not.toBe($scope.name);
+	});
+});
