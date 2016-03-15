3db1eef760671d1d2229cd03bcbfbd3a40f647f4
jakebrady5 <jakebrady5@gmail.com>
2016-03-14 23:37:08 -0400

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,8 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope, $timeout) {
+	$scope.name = 'jake';
+  $timeout(function(){
+    $scope.name = 'judy';
+  }, 5000);
 }
 
 angular
