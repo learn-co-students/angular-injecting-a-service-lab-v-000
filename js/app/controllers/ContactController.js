eb38cec9f40e34bf032d9d8da7979d51baadf7a9
febbraiod <donedesign1@gmail.com>
2016-05-08 15:33:36 -0400

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,9 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope, $timeout) {
+	$scope.name = 'Don';
+
+  $timeout(function(){
+    $scope.name ='Mimi';
+  }, 5000);
 }
 
 angular
