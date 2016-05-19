628874e4ac19eea529c9b63976dc2ee8f94c2460
authorbeard <msnolan@gmail.com>
2016-05-19 08:50:36 -0400

    all specs passing

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,8 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope, $timeout) {
+	$scope.name = 'Michael';
+  $timeout(function(){
+    $scope.name = 'authorbeard'
+  }, 5000)
 }
 
 angular
