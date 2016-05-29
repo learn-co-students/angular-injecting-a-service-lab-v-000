6cc48921116cf5439e1b4d54545b917ac86f40d3
genericlady <yiannakokalas@gmail.com>
2016-05-29 13:01:00 -0400

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,9 @@
-function ContactController($scope) {
+function ContactController($scope, $timeout) {
 	$scope.name = 'Bill Gates';
+
+	$timeout(function() {
+		$scope.name = 'foobar';
+	}, 5000);
 }
 
 angular
