eb09b0e99dca02c5343dcc5131e013444b0be4e5
coltonstaab1 <colton.staab@gmail.com>
2016-06-06 20:23:24 -0400

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,9 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope, $timeout) {
+	$scope.name = 'Colton Staab';
+  $timeout(function () {
+    $scope.name = 'Matthew Staab';
+  }, 5000);
+
 }
 
 angular
