3fefb79fec9bbc29dc300b70e6e0dfd710cffd86
Tyler Smith <oats000@hotmail.com>
2016-06-17 01:59:53 +0000

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,9 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope, $timeout) {
+	$scope.name = 'Tyler Smith';
+	$timeout(function () {
+    // this would be fired after 2 seconds!
+		$scope.name = 'Not Tyler'
+	}, 5000);
 }
 
 angular
