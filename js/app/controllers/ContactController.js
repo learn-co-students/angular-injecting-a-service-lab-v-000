63ad09c2eacbd844967c8310d83057fcf63a7728
matthewpaulmcgowan <matthewpaulmcgowan@gmail.com>
2016-06-19 14:53:28 -0500

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,8 @@
-function ContactController($scope) {
+function ContactController($scope,$timeout) {
 	$scope.name = 'Bill Gates';
+	$timeout(function(){
+	  $scope.name = "Yo Yo Ma";
+	}, 5000);
 }
 
 angular
