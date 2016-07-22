bc627411f6378483be98ac7354b4304e612b70ea
thebrianemory <the@brianemory.com>
2016-07-22 15:16:38 -0700

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,9 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope, $timeout) {
+	$scope.name = 'Brian';
+
+  $timeout(function () {
+    $scope.name = 'Not Brian';
+  }, 5000);
 }
 
 angular
