3947977902fcf12b77d55085aef88d3659020e79
Peter Jacobson <github email address>
2016-08-03 12:21:57 -0400

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,11 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope, $timeout) {
+	$scope.name = 'Peter Jacobson';
+
+
+$timeout(function () {
+  $scope.name = 'Morrissey';
+}, 5000);
+
 }
 
 angular
