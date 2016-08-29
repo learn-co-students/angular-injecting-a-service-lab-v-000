79e15fb383264f30319f508e1a4e30375d21cfa2
James Novak <jamesnovak90@gmail.com>
2016-08-28 20:29:12 -0400

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,9 @@
-function ContactController($scope) {
+function ContactController($scope, $timeout) {
 	$scope.name = 'Bill Gates';
+
+  $timeout(function(){
+    $scope.name = "sam sepi0l"
+  }, 5000)
 }
 
 angular
