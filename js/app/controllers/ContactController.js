218ab49b6bcf8da17f3cc2d3aa9694ba4ff1b5de
agdavid <antonio.g.david@gmail.com>
2016-08-30 08:15:21 -0400

    inject service into controller

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,7 +1,17 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope, $timeout) {
+	// set initial value of property 'name'
+  $scope.name = 'Antonio David';
+  // inject $timeout service
+  $timeout(
+    function() {
+      // set updated value of property 'name'
+      $scope.name = 'My New Name'
+    }, 5000  
+  );
 }
 
 angular
+  // fetch existing module
 	.module('app')
+  // attach controller to existing module
 	.controller('ContactController', ContactController);
