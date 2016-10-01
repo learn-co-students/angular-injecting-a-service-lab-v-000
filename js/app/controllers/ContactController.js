389273dd1ad24b8d3802accd633c8ef3dbe84f27
Saturn226 <serenehopes@gmail.com>
2016-10-01 14:45:45 -0400

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,12 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope,$timeout) {
+	$scope.name = 'Ashley Muller';
+ 
+      $timeout(function () {
+          $scope.name= "Bill Gates";
+    }, 5000);
+
+
+
 }
 
 angular
