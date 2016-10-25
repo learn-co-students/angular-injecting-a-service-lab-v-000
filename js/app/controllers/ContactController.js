2df6b0f7b6d50b025b73a92da66272fc47dd1dd5
rgylling <mikegylling@yahoo.com>
2016-10-25 14:59:53 -0700

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,9 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope,$timeout) {
+	$scope.name = 'Testing Name';
+
+	$timeout(function (){
+		$scope.name = 'Better Name'
+	},5000);
 }
 
 angular
