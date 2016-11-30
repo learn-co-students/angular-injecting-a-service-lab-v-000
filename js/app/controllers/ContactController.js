bad5efda4df731491ac29ac410d9100ce9862a7a
depippo <jdepippo@gmail.com>
2016-11-30 14:37:15 -0500

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,9 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope, $timeout) {
+	$scope.name = 'John DePippo';
+  
+  $timeout(function() {
+    $scope.name = "Wade Boggs"
+  }, 5000);
 }
 
 angular
