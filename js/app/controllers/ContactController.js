0832f08417ed0b724a98bdc18f64bf32759541e5
DownsideupmediaUSA <brandondecarlo@live.com>
2017-01-04 16:38:05 -0500

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,6 +1,12 @@
-function ContactController($scope) {
+function ContactController($scope, $timeout) {
 	$scope.name = 'Bill Gates';
+
+$timeout(function() {
+	  $scope.name = 'Steve Jobs'
+}, 5000);
+
 }
+
 
 angular
 	.module('app')
