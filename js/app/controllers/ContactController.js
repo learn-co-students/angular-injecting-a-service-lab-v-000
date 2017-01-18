1fe89ff448e9d61ce36ed23a7e2472bcfe3f6a38
Ann Lee <aplee29@users.noreply.github.com>
2017-01-18 00:21:19 -0500

    Done.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,9 @@
-function ContactController($scope) {
-	$scope.name = 'Bill Gates';
+function ContactController($scope, $timeout) {
+	$scope.name = 'Ann Lee';
+
+  $timeout(function() {
+    $scope.name = "Rob Smith";
+  }, 5000);
 }
 
 angular
