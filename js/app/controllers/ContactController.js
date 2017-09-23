5a6ac3f80aec47815089a72a50a278ca18178504
Heber Sandoval <he3ers@gmail.com>
2017-09-23 16:52:14 -0400

    Injected services into controller's parameters.

--- js/app/controllers/ContactController.js
+++ js/app/controllers/ContactController.js
@@ -1,5 +1,10 @@
-function ContactController($scope) {
+function ContactController($scope, $timeout) {
 	$scope.name = 'Bill Gates';
+
+	// using the injected service inside the controller
+	$timeout(function () {
+		$scope.name = 'Mickey Mouse';
+	}, 5000);
 }
 
 angular
